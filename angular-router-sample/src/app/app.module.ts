5382dbfe54857f33455921aa448fba74f6acab84
Marcell Tanure <59565705+marcellpaganini@users.noreply.github.com>
2022-09-02 10:14:50 -0300

    custom route match implemented

--- angular-router-sample/src/app/app.module.ts
+++ angular-router-sample/src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { RouterModule, UrlSegment } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroesListComponent } from './heroes-list/heroes-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CrisisListComponent,
     HeroesListComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
@@ -20,6 +22,21 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
       { path: 'crisis-list', component: CrisisListComponent },
       { path: 'heroes-list', component: HeroesListComponent },
       {path: '', redirectTo: '/heroes-list', pathMatch: 'full'},
+      {
+        matcher: (url) => {
+          if (url.length === 1 && url[0].path.match(/^@[\w]+$/gm)) {
+            return {
+              consumed: url,
+              posParams: {
+                username: new UrlSegment(url[0].path.slice(1), {})
+              }
+            };
+          }
+      
+          return null;
+        },
+        component: ProfileComponent
+      },
       {path: '**', component: PageNotFoundComponent}
     ])
   ],
